e89430cffcc2cffc1087605913100f2a559ffc07
Olivier BOUDJEMA <olivier.boudjema@gmail.com>
2017-07-22 14:59:23 +0200

    1

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("slt");
+'use strict'
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res) {
+  res.send("Hi i'm a chatbot!")
+})
+
+app.get('/webhook/', function(req, res) {
+  if (req.query['hub.verify_token'] === "mdp") {
+    res.send(req.query['bug.challenge']);
+  }
+  res.send("wrong token");
+})
+
+app.listen(app.get('port'), function(){
+  console.log("running: port")
+})
