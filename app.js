fc74723b531b49897e4158171168d77c35909937
Olivier BOUDJEMA <olivier.boudjema@gmail.com>
2017-07-23 15:06:36 +0200

    fbtest

--- app.js
+++ app.js
@@ -19,50 +19,121 @@ app.get('/', function(req, res) {
 	console.log("teeeeeeeeeeeeeeeeeest 1");
 })
 
-let token = ""
+//let access_token = "EAABwYHYpXLoBAHjdMpnud6mghexpMxPWPN5scNGTlYbLXOTRDxLWHThkJo3i0ZCZCWvQBfhTuYZAxyRRbWRsBUEqFI4kH71n4f4olvjPUDbhethAEcvejWjZCQWeWm9i5si5m6lXHYbtBouD4hZBD2KIbJ6IhV39CEe3EoBxPGgZDZD"
 
 // Facebook
 
-app.get('/webhook/', function(req, res) {
-	console.log("teeeeeeeeeeeeeeeeeest 2");
-	if (req.query['hub.verify_token'] === "chatbot") {
-		res.send(req.query['hub.challenge'])
-	}
-	res.send("Wrong token")
-})
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === <chatbot>) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
 
-app.post('/webhook/', function(req, res) {
-	console.log("teeeeeeeeeeeeeeeeeest 3");
-	let messaging_events = req.body.entry[0].messaging
-	for (let i = 0; i < messaging_events.length; i++) {
-		let event = messaging_events[i]
-		let sender = event.sender.id
-		if (event.message && event.message.text) {
-			let text = event.message.text
-			sendText(sender, "Text echo: " + text.substring(0, 100))
-		}
-	}
-	res.sendStatus(200)
-})
+app.post('/webhook', function (req, res) {
+  var data = req.body;
 
-function sendText(sender, text) {
-	let messageData = {text: text}
-	console.log("teeeeeeeeeeeeeeeeeest 4");
-	request({
-		url: "https://graph.facebook.com/v2.6/me/messages",
-		qs : {access_token: token},
-		method: "POST",
-		json: {
-			recipient: {id: sender},
-			message : messageData,
-		}
-	}, function(error, response, body) {
-		if (error) {
-			console.log("sending error")
-		} else if (response.body.error) {
-			console.log("response body error")
-		}
-	})
+  // Make sure this is a page subscription
+  if (data.object === 'page') {
+
+    // Iterate over each entry - there may be multiple if batched
+    data.entry.forEach(function(entry) {
+      var pageID = entry.id;
+      var timeOfEvent = entry.time;
+
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.message) {
+          receivedMessage(event);
+        } else {
+          console.log("Webhook received unknown event: ", event);
+        }
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
+  }
+});
+
+function receivedMessage(event) {
+  // Putting a stub for now, we'll expand it in the following steps
+  console.log("Message data: ", event.message);
+}
+
+function receivedMessage(event) {
+  var senderID = event.sender.id;
+  var recipientID = event.recipient.id;
+  var timeOfMessage = event.timestamp;
+  var message = event.message;
+
+  console.log("Received message for user %d and page %d at %d with message:",
+    senderID, recipientID, timeOfMessage);
+  console.log(JSON.stringify(message));
+
+  var messageId = message.mid;
+
+  var messageText = message.text;
+  var messageAttachments = message.attachments;
+
+  if (messageText) {
+
+    // If we receive a text message, check to see if it matches a keyword
+    // and send back the example. Otherwise, just echo the text we received.
+    switch (messageText) {
+      case 'generic':
+        sendGenericMessage(senderID);
+        break;
+
+      default:
+        sendTextMessage(senderID, messageText);
+    }
+  } else if (messageAttachments) {
+    sendTextMessage(senderID, "Message with attachment received");
+  }
+}
+
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText
+    }
+  };
+
+  callSendAPI(messageData);
+}
+
+function callSendAPI(messageData) {
+  request({
+    uri: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: { access_token: "EAABwYHYpXLoBAHjdMpnud6mghexpMxPWPN5scNGTlYbLXOTRDxLWHThkJo3i0ZCZCWvQBfhTuYZAxyRRbWRsBUEqFI4kH71n4f4olvjPUDbhethAEcvejWjZCQWeWm9i5si5m6lXHYbtBouD4hZBD2KIbJ6IhV39CEe3EoBxPGgZDZD" },
+    method: 'POST',
+    json: messageData
+
+  }, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      var recipientId = body.recipient_id;
+      var messageId = body.message_id;
+
+      console.log("Successfully sent generic message with id %s to recipient %s",
+        messageId, recipientId);
+    } else {
+      console.error("Unable to send message.");
+      console.error(response);
+      console.error(error);
+    }
+  });
 }
 
 app.listen(app.get('port'), function() {
