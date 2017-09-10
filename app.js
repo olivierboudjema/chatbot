eb8208a151096b9d9f37c658a8bbdc1e403332f5
Olivier BOUDJEMA <olivier.boudjema@gmail.com>
2017-09-10 20:37:51 +0200

    rrr

--- app.js
+++ app.js
@@ -85,12 +85,12 @@ app.post('/webhook', function(req, res) {
 			//sendText(sender, "" + text.substring(0, 100))
       for (var j in users.user) {
           if(users.user[j].id == event.sender.id){
-            sendText(sender, "" + text.substring(0, 100));
-            sendText(console.log(users.user[i].first_name));
+            sendText(sender, "" + text.substring(0, 100));            
             compteur++;
           }
+          sendText(sender, users.user[i].first_name));
       }
-      if(compteur == 1) {
+      if(compteur <= 2) {
         sendText(sender, "" + "Hi " + info.toString() + "!!!")
         users.user.push({  "id": users.user[j].id, "first_name":users.user[j].first_name, "last_name": users.user[j].last_name })
         compteur = 0;
